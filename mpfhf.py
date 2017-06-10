31c086edfac1f29e7a00e3aba2dda258edf88c1d
Sina Sadeghi <sina.sa@gmail.com>
2017-06-10 13:04:48 +1000

    fixes

--- mpfhf.py
+++ mpfhf.py
@@ -13,7 +13,7 @@ def invert(field):
 
 def screw(a, b, M_pos, half):
     count = len(a)/2 if half else len(a)
-    for i in range(0, len(a)):
+    for i in range(0, count):
         flip(b, ((i * M_pos) % len(b)))
 
 def expand():
@@ -22,30 +22,30 @@ def expand():
 message = argv[1]
 R_len = int(argv[2])
 
-M = map(int, ''.join(map(lambda p: format(p, 'b'), map(ord, message))))
+M = list(map(int, ''.join(map(lambda p: format(p, 'b'), map(ord, message)))))
 S = [0]
 R = [0] * R_len
-
 M_pos = 0
+step = 0
+
 while M_pos < len(M):
     if M[M_pos] == 0:
         expand()
         screw(S, R, M_pos, False)
         if R[M_pos%R_len] == 0:
             flip(R, M_pos%R_len)
-            if M_pos != 0:
-                M_pos -= 1
+            if M_pos != 0: M_pos -= 1
         else:
             flip(R, M_pos%R_len)
             invert(S)
     else:
         screw(S, R, M_pos, True)
-        S_len = len(S)
-        if R[M_pos%R_len] == S[M_pos%S_len]:
+        if R[M_pos%R_len] == S[M_pos%len(S)]:
             expand()
             screw(R, S, M_pos, False)
         else:
             flip(R, M_pos%R_len)
     M_pos += 1
+    step += 1
 
 print(stringify(R))
